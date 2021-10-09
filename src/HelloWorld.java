f4a51053bfe573154cfc4e73c90959a17d57afd4
joosangryang <wntkdfid@gmail.com>
2021-10-09 18:32:11 +0900

    [update] switch문 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,22 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World!!");
+		
+		int n = 3;
+		
+		switch(n)
+		{
+		case 1:
+			System.out.println("Siple Java");
+		case 2:
+			System.out.println("Funny Java");
+		case 3:
+			System.out.println("Fantastic Java");
+		default:
+			System.out.println("Do You like Coffee ?");
+		}
+		
+		System.out.println("Do You like Orange");
 
 	}
 
