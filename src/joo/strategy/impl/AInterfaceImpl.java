589e7d1e9e46e0bec4b3db37877074277878489f
joosangryang <wntkdfid@gmail.com>
2021-10-10 23:43:30 +0900

    initial strategy 구현

---
+++ src/joo/strategy/impl/AInterfaceImpl.java
@@ -0,0 +1,14 @@
+package joo.strategy.impl;
+
+import joo.strategy.inf.AInterface;
+
+public class AInterfaceImpl implements AInterface {
+
+	@Override
+	public void funcA() {
+		
+		System.out.println("AAA");
+
+	}
+
+}
