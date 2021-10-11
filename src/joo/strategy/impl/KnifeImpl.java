da86d2f2b03d12f2bcef38d8ab22390529a884bf
joosangryang <wntkdfid@gmail.com>
2021-10-11 22:54:37 +0900

    initial [strtegy pattern] KnifeImpl

---
+++ src/joo/strategy/impl/KnifeImpl.java
@@ -0,0 +1,14 @@
+package joo.strategy.impl;
+
+import joo.strategy.inf.WeaponInf;
+
+public class KnifeImpl implements WeaponInf {
+
+	@Override
+	public void attack() {
+		
+		System.out.println("Knife attack");
+
+	}
+
+}
