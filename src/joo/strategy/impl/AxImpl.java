c4f35768db17fe17737b54b30a1655aa356d8e20
joosangryang <wntkdfid@gmail.com>
2021-10-11 22:52:38 +0900

    initial [strategy pattern] Aximpl

---
+++ src/joo/strategy/impl/AxImpl.java
@@ -0,0 +1,14 @@
+package joo.strategy.impl;
+
+import joo.strategy.inf.WeaponInf;
+
+public class AxImpl implements WeaponInf {
+
+	@Override
+	public void attack() {
+		
+		System.out.println("Ax attack");
+
+	}
+
+}
