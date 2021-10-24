c399c696a00192757e1436e6e982b4f268f890c6
joosangryang <wntkdfid@gmail.com>
2021-10-24 23:23:10 +0900

    initial factory pattern impl

---
+++ src/joo/factory/impl/CircleImpl.java
@@ -0,0 +1,13 @@
+package joo.factory.impl;
+
+import joo.factory.inf.ShapeInf;
+
+public class CircleImpl implements ShapeInf {
+
+	@Override
+	public void draw() {
+		System.out.println("Circle draw Method");
+
+	}
+
+}
