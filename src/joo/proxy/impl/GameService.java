e74f7d9d960823959b0d1bea8064257788c07743
joosangryang <wntkdfid@gmail.com>
2021-11-01 19:53:56 +0900

    initial proxy interface

---
+++ src/joo/proxy/impl/GameService.java
@@ -0,0 +1,11 @@
+package joo.proxy.impl;
+
+public class GameService {
+	
+	public void startGame() throws InterruptedException {
+		
+		System.out.println("이자리에 오신 여러분을 진심으로 환영합니다.");
+		Thread.sleep(1000L);
+	}
+
+}
